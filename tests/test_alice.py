ddd75f97bfb199c80a4ab8bea8424829f4a6ae1f
nastya-asya <61165064+nastya-asya@users.noreply.github.com>
2021-02-01 15:26:21 +0300

    Update test_alice.py

--- tests/test_alice.py
+++ tests/test_alice.py
@@ -3,8 +3,6 @@
 import re
 from src.main import session_data
 
-user_id = "47C73714B580ED2469056E71081159529FFC676A4E5B059D629A819E857DC2F8"
-
 
 def test_hello(client):
     response = client.get('/hello/world')
@@ -30,13 +28,13 @@ def test_2(client):
             "message_id": 0,
             "session_id": "2eac4854-fce721f3-b845abba-20d60",
             "skill_id": "3ad36498-f5rd-4079-a14b-788652932056",
-            "user_id": user_id,
+            "user_id": "47C73714B580ED2469056E71081159529FFC676A4E5B059D629A819E857DC2F8",
             "user": {
                 "user_id": "6C91DA5198D1758C6A9F63A7C5CDDF09359F683B13A18A151FBF4C8B092BB0C2",
                 "access_token": "AgAAAAAB4vpbAAApoR1oaCd5yR6eiXSHqOGT8dT"
             },
             "application": {
-                "application_id": user_id
+                "application_id": "47C73714B580ED2469056E71081159529FFC676A4E5B059D629A819E857DC2F8"
             },
             "new": True
         },
@@ -66,13 +64,13 @@ def test_3(client):
             "message_id": 1,
             "session_id": "2eac4854-fce721f3-b845abba-20d60",
             "skill_id": "3ad36498-f5rd-4079-a14b-788652932056",
-            "user_id": user_id,
+            "user_id": "47C73714B580ED2469056E71081159529FFC676A4E5B059D629A819E857DC2F8",
             "user": {
                 "user_id": "6C91DA5198D1758C6A9F63A7C5CDDF09359F683B13A18A151FBF4C8B092BB0C2",
                 "access_token": "AgAAAAAB4vpbAAApoR1oaCd5yR6eiXSHqOGT8dT"
             },
             "application": {
-                "application_id": user_id
+                "application_id": "47C73714B580ED2469056E71081159529FFC676A4E5B059D629A819E857DC2F8"
             },
             "new": False
         },
@@ -104,13 +102,13 @@ def test_4(client):
             "message_id": 1,
             "session_id": "2eac4854-fce721f3-b845abba-20d60",
             "skill_id": "3ad36498-f5rd-4079-a14b-788652932056",
-            "user_id": user_id,
+            "user_id": "47C73714B580ED2469056E71081159529FFC676A4E5B059D629A819E857DC2F8",
             "user": {
                 "user_id": "6C91DA5198D1758C6A9F63A7C5CDDF09359F683B13A18A151FBF4C8B092BB0C2",
                 "access_token": "AgAAAAAB4vpbAAApoR1oaCd5yR6eiXSHqOGT8dT"
             },
             "application": {
-                "application_id": user_id
+                "application_id": "47C73714B580ED2469056E71081159529FFC676A4E5B059D629A819E857DC2F8"
             },
             "new": False
         },
@@ -141,13 +139,13 @@ def test_5(client):
             "message_id": 1,
             "session_id": "2eac4854-fce721f3-b845abba-20d60",
             "skill_id": "3ad36498-f5rd-4079-a14b-788652932056",
-            "user_id": user_id,
+            "user_id": "47C73714B580ED2469056E71081159529FFC676A4E5B059D629A819E857DC2F8",
             "user": {
                 "user_id": "6C91DA5198D1758C6A9F63A7C5CDDF09359F683B13A18A151FBF4C8B092BB0C2",
                 "access_token": "AgAAAAAB4vpbAAApoR1oaCd5yR6eiXSHqOGT8dT"
             },
             "application": {
-                "application_id": user_id
+                "application_id": "47C73714B580ED2469056E71081159529FFC676A4E5B059D629A819E857DC2F8"
             },
             "new": False
         },
@@ -179,13 +177,13 @@ def test_6(client):
             "message_id": 1,
             "session_id": "2eac4854-fce721f3-b845abba-20d60",
             "skill_id": "3ad36498-f5rd-4079-a14b-788652932056",
-            "user_id": user_id,
+            "user_id": "47C73714B580ED2469056E71081159529FFC676A4E5B059D629A819E857DC2F8",
             "user": {
                 "user_id": "6C91DA5198D1758C6A9F63A7C5CDDF09359F683B13A18A151FBF4C8B092BB0C2",
                 "access_token": "AgAAAAAB4vpbAAApoR1oaCd5yR6eiXSHqOGT8dT"
             },
             "application": {
-                "application_id": user_id
+                "application_id": "47C73714B580ED2469056E71081159529FFC676A4E5B059D629A819E857DC2F8"
             },
             "new": False
         },
